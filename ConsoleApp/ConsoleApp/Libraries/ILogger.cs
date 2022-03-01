ec9fa700fde39d74076eebd1bf6ef336b809c4e0
abrehm264 <EntApp_DevPortal@e-hps.com>
2022-03-01 13:42:58 -0500

    OctopusDeploy release: 0.1.0.21-main

--- ConsoleApp/ConsoleApp/Libraries/ILogger.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-namespace ConsoleApp.Libraries
-{
-    public enum LogLevels { Verbose, Warn, Error }
-    public interface ILogger
-    {
-        public void Log(LogLevels level, string message);
-    }
-}
